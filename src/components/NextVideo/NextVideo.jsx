3775f43912d5d304d7fece9f692748f6c40326dd
winnier21 <you@example.com>
2024-06-08 19:01:26 -0700

    next video update

--- src/components/NextVideo/NextVideo.jsx
+++ src/components/NextVideo/NextVideo.jsx
@@ -1,30 +1,24 @@
-import React from 'react';
-import './NextVideo.scss';
-
+import React from "react";
+import "./NextVideo.scss";
 
 const NextVideo = ({ onSelectVideo, videoData, selectedVideoId }) => {
-
-    return (
-        <ul>
-            {videoData.map((video) => {
-            return (
-                // key={video.id}
-                // id={video.id}
-                <li>
-                    <img className ="nextVideo__thumbnails" src={video.image} ></img>
-                    title={video.title}
-                    channel={video.channel}
-                    // onSelectVideo={onSelectVideo}
-                    // selectedVideoId={selectedVideoId}
-                </li>
-            )
-            })}
-
-        </ul>
-    
-        
+  return (
+    <ul>
+      {videoData.map((video) => {
+        return (
+          // key={video.id}
+          // id={video.id}
+          <li>
+            <img className="nextVideo__thumbnails" src={video.image}></img>
+            <h2>{video.title}</h2>
+            <h3>{video.channel}</h3>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
-
-export default NextVideo
+export default NextVideo;
+/* // onSelectVideo={onSelectVideo}
+                    // selectedVideoId={selectedVideoId} */
