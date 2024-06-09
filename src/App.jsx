b284a61edd42dd7fa77971aefb1cb5b8a85c5122
winnier21 <you@example.com>
2024-06-08 17:51:46 -0700

    main video jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import './styles/App.scss';
+import React, { useState } from "react";
+import videoData from "./data/video-details.json";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import MainVideo from "./components/MainVideo/MainVideo.jsx";
+import "../src/App.scss";
 
 function App() {
+  const [currentVideo, setCurrentVideo] = useState(videoData[0]);
+
+  const selectVideo = (videoId) => {
+    console.log('hello', videoId);
+    const selectedVideo = videoData.find((video) => video.id === videoId);
+    setCurrentVideo(selectedVideo);
+  };
+
   return (
     <>
-    <Navbar />
+    <div>
+      <Navbar />
+      <MainVideo videoData={currentVideo} />
+    </div>
+    </>
+  );
+};
 
+export default App;
 
-</>
-  )
-}
-
-export default App
