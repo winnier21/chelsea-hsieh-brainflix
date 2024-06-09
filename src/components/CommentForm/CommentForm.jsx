26895ab30a3e81ca4cccc835533c9163885a5abb
winnier21 <you@example.com>
2024-06-09 13:27:48 -0700

    update: comment form styling

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import './CommentForm.scss';
-import avatar from '../../assets/Images/Mohan-muruge.jpg';
-import commentIcon from '../../assets/Icons/add_comment.svg';
+import React from "react";
+import "./CommentForm.scss";
+import avatar from "../../assets/Images/Mohan-muruge.jpg";
+import commentIcon from "../../assets/Icons/add_comment.svg";
 
-
-const CommentForm =() => {
+const CommentForm = () => {
   return (
-    
-    <form className='comment-form'>
-        <div >
-            <img className='comment-form__avatar' src={avatar} alt="avatar"/>
-        </div>
-        <label htmlFor='comment' className='comment-form__label'>JOIN THE CONVERSATION</label>
-        <input className='comment-form__input' placeholder='Add a new comment'></input>
-        <div class="comment-form__button">
-            <img className='comment-form__button--icon' src={commentIcon} alt="comment button" />
-            <button className='comment-form__button--trigger'>COMMENT</button>
-        </div>
-        </form>
-  )
-}
+    <form className="comment-form">
+      <div className="comment-form__left">
+        <img className="comment-form__avatar" src={avatar} alt="avatar" />
+      </div>
+      <div className="comment-form__right">
+        <label htmlFor="comment" className="comment-form__label">
+          JOIN THE CONVERSATION
+        </label>
+        <textarea
+          className="comment-form__textarea"
+          placeholder="Add a new comment"
+        ></textarea>
 
-export default CommentForm
+        <div class="comment-form__button">
+          <img
+            className="comment-form__icon"
+            src={commentIcon}
+            alt="comment button"
+          />
+          <button className="comment-form__trigger">COMMENT</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default CommentForm;
