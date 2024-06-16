fa83f2e12031b42029271903609fc4ea1fe0c7ef
winnier21 <you@example.com>
2024-06-16 14:31:11 -0700

    upload page style mobile setup

--- src/components/VideoUpload/VideoUpload.jsx
+++ src/components/VideoUpload/VideoUpload.jsx
@@ -1,50 +1,4 @@
-// import React from 'react';
-// import videoThumbnail from '../../assets/Images/Upload-video-preview.jpg';
-// import uploadIcon from '../../assets/Icons/publish.svg';
 
-
-// const VideoUpload = () => {
-//     return (
-//       <div class="upload">
-//         <h1>Upload Video</h1>
-//         <h2>Video Thumbnail</h2>
-//         <img className="upload__thumbnail" src={videoThumbnail} alt="video Thumbnail" />
-//         <div class="titleVideo">
-//           <h2>TITLE YOUR VIDEO</h2>
-//           <div class="upload__title">
-//             <input
-//               id="video-title"
-//               type="text"
-//               placeholder="Add a title to your video"
-//             />
-//           </div>
-//         </div>
-//         <div class="titleVideo">
-//           <h2>ADD A VIDEO DESCRIPTION</h2>
-//           <div class="upload__description">
-//             <input
-//               id="video-description"
-//               type="text"
-//               placeholder="Add a discription to your video"
-//             />
-//           </div>
-//         </div>
-  
-//         <div class="publish">
-//           <button className="publish__button">
-//             <img className="publish__icon" src={uploadIcon} alt="publish Icon" />
-//             PUBLISH
-//           </button>
-//           <button className="cancel__button">
-//             CANCEL
-//           </button>
-//         </div>
-//         <hr className="divider" />
-//       </div>
-      
-//     );
-//   };
-// export default VideoUpload
 import React, { useState } from 'react';
 import videoThumbnail from '../../assets/Images/Upload-video-preview.jpg';
 import uploadIcon from '../../assets/Icons/publish.svg';
@@ -76,7 +30,7 @@ const VideoUpload = ({ handleFormSubmit }) => {
         <div className="titleVideo">
           <h2>ADD A VIDEO DESCRIPTION</h2>
           <div className="upload__description">
-            <input
+            <textarea
               id="video-description"
               type="text"
               placeholder="Add a description to your video"
@@ -90,13 +44,13 @@ const VideoUpload = ({ handleFormSubmit }) => {
         <div className="publish">
           <button className="publish__button" type="submit">
             <img className="publish__icon" src={uploadIcon} alt="Publish Icon" />
-            PUBLISH
+            <h2>PUBLISH</h2>
           </button>
           <button className="cancel__button" type="button" onClick={() => window.history.back()}>
-            CANCEL
+          <h2>CANCEL</h2>
           </button>
         </div>
-        <hr className="divider" />
+
       </form>
     </div>
   );
